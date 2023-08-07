0ac4d32e461a8ec2c0afc02e635eb255fc23268b
wendahdesmond <wendahdesmond@gmail.com>
2023-08-07 03:53:40 -0700

    modified the home page

--- ui/src/views/HomePage.vue
+++ ui/src/views/HomePage.vue
@@ -4,25 +4,30 @@
       <div class="container">
         <div class="row px-md-5">
           <div class="col-md-6 p-5">
-            <p class="h1 px-md-4 text-light" style="font-family: 'Raleway', sans-serif;">Speak English Just Like a Native</p>
+            <!-- <p class="h1 px-md-4 text-light" style="font-family: 'Raleway', sans-serif;">Speak English Just Like a Native</p> -->
+            <span class="h1 px-md-4 text-light" style="font-family: 'Raleway', sans-serif;">Speak English Just</span> <br>
+            <span class="h1 px-md-4 text-light" style="font-family: 'Raleway', sans-serif;"> Like a Native</span>
             <p class="px-md-4 text-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
             tempor incididunt ut labore et dolore magna aliqua.</p>
           </div>
-          <div class="col-md-6 py-5 ps-md-5">
-            <a class="btn primary-button m-5 px-5 py-3">GET STARTED</a>
+          <div class="col-md-6 pt-md-5 pb-3">
+            <div class="d-flex align-items-center justify-content-center pt-md-5">
+              <a class="btn primary-button  px-md-5 py-md-3 p-2">GET STARTED</a>
+            </div>
           </div>
         </div>
       </div>
     </section>
     <section class="p-md-3 pt-5">
       <div class="container">
-        <div class="row pt-md-5">
-          <div class="col-md-6">
-            <p class="h2 px-md-5 pt-md-5" style="font-family: 'Raleway', sans-serif;">English for Daily <span style="border-bottom: 5px solid #A01FEF;">Conversation</span></p>
-            <p class="px-md-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        <div class="d-flex flex-column flex-md-row align-items-center justify-conten-between pt-md-5">
+          <div class="col-md-6 ps-md-5">
+            <span class="h2 ps-md-5" style="font-family: 'Raleway', sans-serif;">English for Daily</span> <br>
+            <span class="h2 ms-md-5 pt-md-5" style="font-family: 'Raleway', sans-serif; border-bottom: 5px solid #A01FEF;"> Conversation</span>
+            <p class="px-md-5 mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
           </div>
-          <div class="col-md-6 px-md-5">
+          <div class="col-md-6 px-md-3 mb-3">
             <div class="rounded-4 p-5 file-drag mx-md-4 text-center text-light" style="background-color: rgba(0, 0, 0, 0.7); background-blend-mode: multiply; background-position: center; background-size: cover;">
               <p class="h1 p-5 text-light"><i class="bi-play-fill"></i></p>
               <p class="text-light pt-3">Being in our live class</p>
@@ -39,32 +44,22 @@
             </div>
           </div>
           <div class="col-md-6">
-            <p class="h2 px-md-5 mx-md-5" style="font-family: 'Raleway', sans-serif;">Unlock your professional English</p>
-            <p class="px-md-5 mx-md-5">The quick brown fox scores the whatever Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            <span class="h2 px-md-5 mx-md-5" style="font-family: 'Raleway', sans-serif;">Unlock your</span> <br>
+            <span class="h2 px-md-5 mx-md-5" style="font-family: 'Raleway', sans-serif;"> professional English</span>
+            <p class="px-md-5 mx-md-5 my-4 fw-bold">The quick brown fox scores the whatever Lorem ipsum dolor sit amet, consectetur adipisicing elit.
             quis.</p>
 
-            <p class="px-md-5 mx-md-5">
-              <span class="primary-text h5">
-                <i class="bi-check-circle-fill"></i>
-              </span> <small>Lorem ipsum dolor sit amet.</small>
-            </p>
-            <p class="px-md-5 mx-md-5">
-              <span class="primary-text h5">
-                <i class="bi-check-circle-fill"></i>
-              </span> <small>Lorem ipsum dolor sit amet.</small>
-            </p>
-            <p class="px-md-5 mx-md-5">
-              <span class="primary-text h5">
-                <i class="bi-check-circle-fill"></i>
-              </span> <small>Lorem ipsum dolor sit amet.</small>
-            </p>
-            <p class="px-md-5 mx-md-5">
-              <span class="primary-text h5">
-                <i class="bi-check-circle-fill"></i>
-              </span> <small>Lorem ipsum dolor sit amet.</small>
-            </p>
+            <div v-for="i in 5" :key="i" class="d-flex align-items-center px-md-5 mx-md-5">
+             <span class="h5">
+              <i class="bi-check-circle-fill me-2 primary-text"></i>
+             </span>
+             <span style="margin-top: -10px;">
+              <small>Lorem ipsum dolor sit amet.</small>
+             </span>
+            </div>
 
-            <p class="px-md-5 mx-md-5">
+           
+            <p class="px-md-5 mx-md-5 mt-3">
               <a href="" class="btn btn-lg text-light" style="background: #490194; z-index: 122;">Get Started <i class="bi-arrow-right-circle ms-3 me-2"></i></a>
               <a href="" class="btn btn-lg btn-light primary-text primary-border px-3" style="margin-left: -15px; background: #fff;">Learn More</a>
             </p>
@@ -72,7 +67,7 @@
         </div>
       </div>
     </section>
-    <section class="" style="overflow: hidden;">
+    <section class="container" style="overflow: hidden;">
       <div class="row">
         <div class="col-md-7 row p-5 " style="background: #A01FEF;">
           <div class="col-md-5 pt-4">
@@ -91,7 +86,7 @@
                 <i class="bi-caret-left-fill"></i>
               </span>
             </div>
-            <div class="col-9 px-5"><p class="h2 text-light px-5 ">The Way to Good English</p></div>
+            <div class="col-9 px-md-5"><p class="h2 text-light px-5 mt-3 mt-md-0">The Way to Good English</p></div>
             <div class="col-12 text-center">
               <a class="btn primary-button px-4 py-2 mt-5 mb-4">TAKE THIS COURSE</a>
             </div>
@@ -99,10 +94,10 @@
         </div>
       </div>
     </section>
-    <section class="p-md-5">
+    <section class="p-md-5 mt-3 mt-md-3">
       <div class="container">
-        <div class="row">
-          <div class="col-md-7" style="position: relative;">
+        <div class="row mx-2">
+          <div class=" col-12 col-md-7" style="position: relative;">
             <div class="" style="z-index: 155; position: absolute; width: 75%; right: 30%">
               <img class="shadow rounded-2 w-100"  src="@/assets/woman-teaching.jpg" />
             </div>
@@ -113,16 +108,30 @@
               <img class="shadow rounded-2 w-100" src="@/assets/woman-teaching.jpg" />
             </div>
           </div>
-          <div class="col-md-5 p-3">
-            <p class="h2 px-md-5 pt-md-5 text-start" style="font-family: 'Raleway', sans-serif;">Study At Flexible <br><span style="border-bottom: 5px solid #A01FEF;">Schedule</span></p>
+          <div class=" col-12 col-md-5 p-3 mt-5 mt-md-0">
+            <p class="h2 px-md-5 pt-md-5 text-start mt-5 mt-md-0" style="font-family: 'Raleway', sans-serif;">Study At Flexible <br><span style="border-bottom: 5px solid #A01FEF;">Schedule</span></p>
             <p class="text-start px-md-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 
             <a class="btn btn-lg text-light mx-md-5" style="background: #490194;">
               <i class="bi-exclamation-circle-fill me-4"></i> <small>100% customer satisfaction</small>
             </a>
-            <a class="btn btn-lg mt-2 mx-md-5 primary-button-outline">
-              <i class="bi-arrow-repeat me-4"></i> <small class="ms-3">Safe refund policy</small>
+            <a class="btn btn-lg mt-2  primary-button-outline d-flex align-items-end ms-5">
+             <div class="d-flex align-items-start justify-content-start">
+              <span class="h2">
+                <i class="bi-arrow-repeat me-1"></i>
+              </span>
+               <div class="">
+                <span class="">Our Refundment policy policy</span>
+               </div>
+             </div>
+             <div class="d-flex align-items-end  ms-auto" style="margin-bottom: -18px;">
+              <p> <small style="text-decoration: underline;">Read More</small> </p>
+             </div>
             </a>
+            <p class="px-md-5 mx-md-5 mt-5 text-center">
+              <a href="" class="btn btn-lg text-light" style="background: #490194; z-index: 122;">Get Started <i class="bi-arrow-right-circle ms-3 me-2"></i></a>
+             
+            </p>
           </div>
         </div>
       </div>
