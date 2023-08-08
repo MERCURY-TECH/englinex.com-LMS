b5156092aeaa3533f69d71c793510bb141724848
wendahdesmond <wendahdesmond@gmail.com>
2023-08-08 09:40:21 -0700

    adjust the about us page

--- ui/src/views/AboutPage.vue
+++ ui/src/views/AboutPage.vue
@@ -3,12 +3,33 @@
     <section class="p-md-5" >
       <div class="container">
         <div class="row">
-          <div class="col-md-4">
-            <p class="display-3 primary-text">Nice To Meet You!</p>
+          <div class="col-md-5">
+          <div class="d-none d-md-block">
+            <div class="d-flex align-items-center justify-content-center rounded-pill py-3 px-5" style="background-color: #F7EBFF; width: 24rem; height: 24rem;" >
+                 <div class="d-flex align-items-center justify-content-center rounded-pill  p-3" style="background-color: #E5BBFF;width: 22rem; height: 22rem;">
+                  <div class="d-flex align-items-center justify-content-center rounded-pill px-5 py-3 " style="background-color: #A01FEF;width: 20rem; height: 20rem;">
+                  
+                     <h3 class="h2 text-white fw-bold text-center">
+                       NICE TO MEET YOU
+                     </h3>
+                  </div>
+                </div>
+           </div>
           </div>
-          <div class="col-md-8">
-            <p class="h2" style="font-family: 'Raleway', sans-serif;">By Professional English Speakers For All Learners</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+           <div class="d-block d-md-none d-flex align-items-center justify-content-center rounded-pill py-3 px-5" style="background-color: #F7EBFF; width: 20rem; height: 20rem;" >
+                 <div class="d-flex align-items-center justify-content-center rounded-pill  p-3" style="background-color: #E5BBFF;width: 18rem; height: 18rem;">
+                  <div class="d-flex align-items-center justify-content-center rounded-pill px-5 py-3 " style="background-color: #A01FEF;width: 16rem; height: 16rem;">
+                  
+                     <h3 class="h2 text-white fw-bold text-center">
+                       NICE TO MEET YOU
+                     </h3>
+                  </div>
+                </div>
+           </div>
+          </div>
+          <div class="col-md-7">
+            <p class="h2" style="font-family: 'Raleway', sans-serif;">By Professional English Speakers <br> For All Learners</p>
+            <p class="my-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
             consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
@@ -16,9 +37,13 @@
             proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
             <p class="px-md-5 mx-md-5">
-              <a href="" class="btn btn-lg text-light" style="background: #490194; z-index: 122;">Get Started <i class="bi-arrow-right-circle ms-5 me-2"></i></a>
-              <a href="" class="btn btn-lg btn-light primary-text primary-border px-5" style="margin-left: -19px; background: #fff;">See Pricing</a>
+             
+              
             </p>
+            <div class="d-flex flex-column flex-md-row align=items-center justify-content-center pb-3 px-2">
+              <a href="" class="btn btn-lg text-light pe-3 py-2 text-center" style="background: #490194; z-index: 122;">Get Started <span style="opacity: 0;">ffffffff</span></a>
+              <a href="" class="btn btn-lg btn-light primary-text primary-border px-5 mt-2" style="margin-left: -10px; background: #fff;">See Pricing</a>
+            </div>
           </div>
         </div>
       </div>
@@ -27,30 +52,30 @@
       <div class="row">
         <div class="col-md-6">
           <div class="p-md-5" style="background: #A01FEF;">
-            <div class="">
-              <p class="h1 text-light text-center ps-5" style="border-bottom: 5px solid #E5; font-family: Raleway, sans-serif;"> Our Vision </p>
+            <div class="pt-4">
+              <p class="h1 text-light text-center" style="text-decoration: underline; font-family: Raleway, sans-serif;"> Our Vision </p>
             </div>
-            <p class="text-light pt-3 px-md-5"><small>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            <p class="text-light pt-3 px-md-5 mx-4 text-center"><small>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
             consequat.</small></p>
-            <div class="row m-4">
-              <div class="col-6 text-light"><i class="bi-check-circle-fill" style="color: #C4E02F;"></i> lorem ipsum dolor sit</div>
-              <div class="col-6 text-light"><i class="bi-check-circle-fill" style="color: #C4E02F;"></i> lorem ipsum dolor sit</div>
-              <div class="col-6 text-light"><i class="bi-check-circle-fill" style="color: #C4E02F;"></i> lorem ipsum dolor sit</div>
-              <div class="col-6 text-light"><i class="bi-check-circle-fill" style="color: #C4E02F;"></i> lorem ipsum dolor sit</div>
+            <div class="row px-4 px-md-5 align-items-center justify-content-center pb-4">
+              <div class="col-12 col-md-6 text-light text-center"><i class="bi-check-circle-fill" style="color: #C4E02F;"></i> lorem ipsum dolor sit</div>
+              <div class="col-12 col-md-6 text-light text-center"><i class="bi-check-circle-fill" style="color: #C4E02F;"></i> lorem ipsum dolor sit</div>
+              <div class="col-12 col-md-6 text-light text-center"><i class="bi-check-circle-fill" style="color: #C4E02F;"></i> lorem ipsum dolor sit</div>
+              <div class="col-12 col-md-6 text-light text-center"><i class="bi-check-circle-fill" style="color: #C4E02F;"></i> lorem ipsum dolor sit</div>
             </div>
           </div>
-          <div class="p-md-5" style="">
-            <div class="">
+          <div class="px-3 py-3" style="">
+            <div class="text-center">
               <p class="h1 primary-text text-center ps-5" style="border-bottom: 5px solid #E5; font-family: Raleway, sans-serif;"> Our Mission </p>
             </div>
-            <p class="rounded-2 p-md-5 mt-3" style="border: 2px dashed #C4E02F;"><small>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            <p class="rounded-2 p-md-3 mt-3 p-2" style="border: 2px dashed #C4E02F;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
             consequat. Ut enim ad minim veniam,
             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat</small></p>
+            consequat</p>
             
           </div>
         </div>
@@ -106,14 +131,14 @@
             </div>
           </div>
           <div class="col-md-6">
-            <div class="p-md-5 mb-4 rounded-2" style="background: #A01FEF;">
+            <div class="p-md-2 mb-4 rounded-2 px-3" style="background: #A01FEF;">
               <div class="">
                 <p class="h1 text-light text-center ps-5" style="border-bottom: 5px solid #E5; font-family: Raleway, sans-serif;"> Our Team</p>
               </div>
-              <p class="text-light pt-3 px-md-5"><small>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              <p class="text-light py-3 px-md-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-              consequat.</small></p>
+              consequat.</p>
             </div>
             <div class="row">
               <div class="col-md-4 p-4 text-center">
